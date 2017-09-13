0b695d0a4a58cffae6d8b75cb680e61df9743294
web1992 <554933654@qq.com>
2017-09-13 13:26:06 +0800

    antd-mobile

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Login from './login/login';
+import Home from './home/Home';
 import registerServiceWorker from './registerServiceWorker';
-
+import { Button } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css';  // or 'antd-mobile/dist/antd-mobile.less'
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Login />, document.getElementById('root'));
+//ReactDOM.render(<Login />, document.getElementById('root'));
+
+//ReactDOM.render(<Button>Start</Button>, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
